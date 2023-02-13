d672a0afdac7e368de270eb8f2e7378f3664ce3f
Maryam Masoumi <mmasoumi15@gmail.com>
2023-02-13 18:12:32 +0330

    Add Happy Birthday Example and Unit One Menu

--- app/src/main/java/com/masoumi/filime/unit1/HappyBirthday.kt
+++ app/src/main/java/com/masoumi/filime/unit1/HappyBirthday.kt
@@ -3,13 +3,20 @@ package com.masoumi.filime.unit1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.masoumi.filime.R
 import com.masoumi.filime.ui.theme.FilimeTheme
 
 class HappyBirthday : ComponentActivity() {
@@ -21,7 +28,11 @@ class HappyBirthday : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting2("Android")
+                    BirthdayGreetingWithImage(
+                        message = getString(R.string.happy_birthday), from = getString(
+                            R.string.birthday_card_signature
+                        )
+                    )
                 }
             }
         }
@@ -29,14 +40,45 @@ class HappyBirthday : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
+fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = "- from $from",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview2() {
+fun BirthdayCardPreview() {
     FilimeTheme {
-        Greeting2("Android")
+        BirthdayGreetingWithImage("Happy Birthday Maryam!", "Ali")
     }
 }
